08df3ef65cfebf3ddd2ddf498fe3ea5cae17cec3
anvesh2502 <anveshrocks@yahoo.co.in>
2016-07-27 14:32:15 -0500

    Added missing data weighted classifier

--- treepredict.py
+++ treepredict.py
@@ -221,3 +221,30 @@ def prune(tree,mingain) :
               # Merge the branches
               tree.tb,tree.fb=None,None
               tree.results=uniquecounts(tb+fb)
+
+
+def mdclassify(observation,tree) :
+
+    if tree.results!=None :
+        return tree.results
+    else :
+        v=observation[tree.col]
+        if v==None :
+            tr,fr=mdclassify(observation,tree.tb),mdclassify(observation,tree.fb)
+            tcount=sum(tr.values())
+            fcount=sum(fr.values())
+            tw=float(tcount)/(tcount+fcount)
+            fw=float(fcount)/(tcount+fcount)
+            result={}
+            for k,v in tr.items() : result[k]=v*tw
+            for k,v in fr.items() : result[k]=v*fw
+            return result
+        else :
+            if isinstance(v,int) or isinstance(v,float) :
+                if v>=tree.value : branch=tree.tb
+                else : branch=tree.fb
+            else :
+                if v==tree.value : branch=tree.tb
+                else : branch=tree.fb
+
+            return mdclassify(observation,branch)
